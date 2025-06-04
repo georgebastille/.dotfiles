0ff3a505aaba1a223514212d98bbf87cc89f641e
Rich Hanes <georgebastille@gmail.com>
2025-06-04 10:07:18 +0100

    Fix workspaces across monitors

--- pixelbook-arch.sh
+++ pixelbook-arch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S wl-clipboard cliphist ttf-noto-nerd neovim tmux rg fd bat jq fish gnome-keyring libsecret seahorse git base-devel waybar 
+sudo pacman -S wl-clipboard cliphist ttf-noto-nerd neovim tmux rg fd bat jq fish gnome-keyring libsecret seahorse git base-devel waybar tofi cmake meson cpio pkg-config gcc tree bluetui uv ruff pyright mypy
+
+# split-monitor-workspaces plugin for hyprland worspaces
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces # Add the plugin repository
+hyprpm enable split-monitor-workspaces # Enable the plugin
+hyprpm reload # Reload the plugins
